dec31282d7253dbf24ebc65da1fb46fc3d31c757
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2026-02-22 16:11:08 +0200

    Made script a moduel

--- User/Chat/chats.js
+++ User/Chat/chats.js
@@ -2,6 +2,7 @@ import { CONFIG } from "../../Scripts/config.js";
 
 const supabaseUrl = CONFIG.supabaseUrl;
 const supabaseKey = CONFIG.supabaseKey;
+const supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 const usersList = document.getElementById("usersList");
 const logoutBtn = document.getElementById("logoutBtn");
